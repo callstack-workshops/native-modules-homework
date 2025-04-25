21cc54cd7a70e6a9fd847e96457e50314c0e30a8
bartektomczyk <bartlomiejtomczyk@gmail.com>
2025-04-25 11:37:55 +0200

    removed js calls

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -2,9 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import type { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useEffect } from 'react';
-import { NativeModules } from 'react-native';
-import { check, PERMISSIONS } from 'react-native-permissions';
+import React from 'react';
 import { ToastProvider } from 'react-native-toast-notifications';
 
 import { AddLottery } from './screens/AddLottery';
@@ -20,13 +18,7 @@ const options: NativeStackNavigationOptions = {
 
 const queryClient = new QueryClient();
 
-const { Notification } = NativeModules;
-console.log(Notification);
 export const App = () => {
-  useEffect(() => {
-    Notification.requestPermissions();
-    Notification.showNotification('Notification Title', 'Notification Body');
-  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
